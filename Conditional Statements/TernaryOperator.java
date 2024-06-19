cb301d7e9cd634f8f7cc9fa6f0324ce098f7837f
Shrijeet <145208495+Shrijeet345@users.noreply.github.com>
2024-06-19 23:31:04 +0530

    Add files via upload

---
+++ Conditional Statements/TernaryOperator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TernaryOperator{
+    public static void main (String args []){
+        Scanner sc=new Scanner (System.in);
+        int number=sc.nextInt();
+        String type=((number%2)==0)?"Even":"Odd";
+        System.out.println(type);
+
+    }
+}
