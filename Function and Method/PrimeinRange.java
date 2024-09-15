d9a8090f5d4f8047519624401df35eabce81ce97
Shrijeet <145208495+Shrijeet345@users.noreply.github.com>
2024-09-15 09:20:38 +0530

    Update PrimeinRange.java

--- Function and Method/PrimeinRange.java
+++ Function and Method/PrimeinRange.java
@@ -14,12 +14,13 @@ public class PrimeinRange {
     public static void Primerange(int n){
         for (int i=0;i<=n;i++){
             if(isprime(i)){
-                System.out.println(i+" ");
+                System.out.print(i+" ");
             }
         }
     }
     public static void main (String args []){
         Scanner sc=new Scanner (System.in);
         int n=sc.nextInt();
+        Primeinrange(n);
     }
 }
