af455ea7feb9f0e3575eb20e545f60d6ffc127c0
Shrijeet <145208495+Shrijeet345@users.noreply.github.com>
2024-09-15 08:23:22 +0530

    Add files via upload

--- Function and Method/BintoDec.java
+++ Function and Method/BintoDec.java
@@ -1,16 +1,20 @@
 import java.util.*;
 public class BintoDec{
-public static void main (String args []){
-    Scanner sc=new Scanner (System.in);
-    int n=sc.nextInt();
-    int pow=0;
-    int rem=0;
-    while(n>0){
-        int l=n%10;
-        rem=rem+(l*(int)Math.pow(2,pow));
-        pow++;
-        n=n/10;
+    public static int BintoDec(int n){
+        int pow=0;
+        int dec=0;
+        while(n>0){
+            int l=n%10;
+            dec=dec+(l*(int)Math.pow(2,pow));
+            n=n/10;
+            pow++;
         }
-        System.out.println(rem);
-    }   
+        System.out.println(dec);
+        return dec;
+    }
+    public static void main (String args []){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        BintoDec(n);
+    }
 }
--- Function and Method/DectoBin.java
+++ Function and Method/DectoBin.java
@@ -1,16 +1,20 @@
 import java.util.*;
 public class DectoBin{
-public static void main (String args []){
-    Scanner sc=new Scanner (System.in);
-    int n=sc.nextInt();
-    int pow=0;
-    int rem=0;
-    while(n>0){
-        int l=n%2;
-        rem=rem+(l*(int)Math.pow(10,pow));
-        pow++;
-        n=n/2;
+    public static int DectoBin(int n){
+        int pow=0;
+        int dec=0;
+        while(n>0){
+            int l=n%2;
+            dec=dec+(l*(int)Math.pow(10,pow));
+            n=n/2;
+            pow++;
         }
-        System.out.println(rem);
-    }   
+        System.out.println(dec);
+        return dec;
+    }
+    public static void main (String args []){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        DectoBin(n);
+    }
 }
