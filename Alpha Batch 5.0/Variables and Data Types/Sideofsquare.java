ee48fcba350c678005ca2c6ea8d7a85a46d8ddac
Shrijeet <145208495+Shrijeet345@users.noreply.github.com>
2024-10-11 09:29:12 +0530

    Delete Alpha Batch 5.0 directory

--- Alpha Batch 5.0/Variables and Data Types/Sideofsquare.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Sideofsquare {
-    public static void main (String args []){
-        Scanner sc=new Scanner (System.in);
-        int n=sc.nextInt();
-        int side=n*n;
-        System.out.println("Side of a sruare is : "+side);
-    }
-}
