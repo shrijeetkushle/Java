1b1a5ac59f1cc2c7cac5a8a056e8ca7c0c42a452
Shrijeet <145208495+Shrijeet345@users.noreply.github.com>
2024-10-14 23:46:38 +0530

    Add files via upload

---
+++ Imp Questions/Sumoflast2digit.java
@@ -0,0 +1,13 @@
+    import java.util.*;
+public class Sumoflast2digit {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int num1=sc.nextInt(),num2=sc.nextInt();
+		num1=Math.abs(num1);
+		num2=Math.abs(num2);
+		int l1=num1%10;
+		int l2=num2%10;
+		int sum=l1+l2;
+		System.out.println(sum);
+	}
+}
