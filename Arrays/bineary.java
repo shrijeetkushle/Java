a655de42bda9e40d2cf5c7fe5bcc692071e1c34b
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2024-10-23 23:49:31 +0530

    Add files via upload

---
+++ Arrays/bineary.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class bineary{
+    public static int Bineary(int num[],int key){
+        int start=0,end=num.length-1;
+        while(start<=end ){
+            int mid=(start+end)/2;
+        if(num[mid]==key){
+            return mid;
+        }
+        if(num[mid]<key){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+    public static void main (String args []){
+        int arr[]={1,2,3,4,5};
+        int key=5;
+        System.out.println(linear(arr,key));
+    }
+}
