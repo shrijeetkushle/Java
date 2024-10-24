1d342bb501ce7eb2f26b2f3de06ddca932fd939f
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2024-10-24 20:46:13 +0530

    Add files via upload

---
+++ Arrays/reverse.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class reverse{
+    public static void reverse(int num[]){
+        int lar=Integer.MIN_VALUE;
+        int start=0,end=num.length-1;
+        while(start<end){
+            int temp=num[end];
+            num[end]=num[start];
+            num[start]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main (String args []){
+        int arr[]={1,2,3,4,5};
+        reverse(arr);
+        print(arr);
+    }
+}
