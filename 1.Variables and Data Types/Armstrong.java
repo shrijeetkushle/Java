7fc2b6d4d0c8f4853ad3633669dd5250f0d4738b
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-18 01:02:42 +0530

    Update Armstrong.java

--- 1.Variables and Data Types/Armstrong.java
+++ 1.Variables and Data Types/Armstrong.java
@@ -1,16 +1,24 @@
 import java.util.*;
 public class Armstrong{
-    public static void main (String args []){
-        Scanner sc=new Scanner (System.in);
-        int n=sc.nextInt();
+    public static int Sumofpow(int n,int d){
         int sum=0;
         while(n!=0){
             int last=n%10;
-            int cub=(int)Math.pow(last,3);
-            sum+=cub;
+            sum+=Math.pow(last,d);
             n=n/10;
-            
         }
-        System.out.println(sum);
+        return sum;
+    }
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print(" Enter the number : ");
+        int n=sc.nextInt();
+        int digits=String.valueOf(n).length();
+        if(Sumofpow(n,digits)==n){
+            System.out.println("Armstrong Number");
+        }
+        else{
+            System.out.println("Not an Armstrong number");
+        }
     }
 }
