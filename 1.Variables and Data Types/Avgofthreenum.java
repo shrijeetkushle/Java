c4202d2d89c5c4490b212ea5defe937fef300b28
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-18 20:13:36 +0530

    Update Avgofthreenum.java

--- 1.Variables and Data Types/Avgofthreenum.java
+++ 1.Variables and Data Types/Avgofthreenum.java
@@ -8,13 +8,8 @@ public class Avgofthreenum{
         int n2 = sc.nextInt();
         System.out.print("Enter the number 3: ");
         int n3 = sc.nextInt();
-        if (n1 >= n2 && n1 >= n3) {
-            System.out.println("Number one is the greatest");
-        } else if (n2 >= n1 && n2 >= n3) {
-            System.out.println("Number two is the greatest");
-        } else {
-            System.out.println("Number three is the greatest");
-        }
+        double avg=(n1+n2+n3)/3;
+        System.out.println("The average of numbers is :"+avg);
         sc.close();
     }
 }
