3a1f7938132ef4dbbf2b045a09537f184a8daaa8
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-18 19:59:47 +0530

    Update AreaofCircle.java

--- 1.Variables and Data Types/AreaofCircle.java
+++ 1.Variables and Data Types/AreaofCircle.java
@@ -4,6 +4,7 @@ public class AreaofCircle{
         System.out.print("Enter the radius of the circle :");
         Scanner sc=new Scanner (System.in);
         float rad=sc.nextFloat();
+        //PI=3.14
         double area=Math.PI*rad*rad;
         System.out.println(" The Area of circle is + "+area);
 
