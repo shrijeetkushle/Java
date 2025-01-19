3ad3b9344cc475698c2fda97a787c16450395a65
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-20 00:34:43 +0530

    Update and rename FactorialCalculator.java to FactorialCalculator.java

--- 5.Function and Method/FactorialCalculator.java
+++ 5.Function and Method/FactorialCalculator.java
@@ -8,9 +8,10 @@ public class FactorialCalculator{
         System.out.println(fact);
         return fact;
     }
-    public static void main (String args []){
+    public static void main (String[] args){
         Scanner sc=new Scanner (System.in);
         int n=sc.nextInt();
         Factorial(n);
     }
+    sc.close();
 }
