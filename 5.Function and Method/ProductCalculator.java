50f9ee7d0e0e9a38d2b76b31750dbbee67ec5031
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-20 00:37:20 +0530

    Update and rename ProductCalculator.java to ProductCalculator.java

--- 5.Function and Method/ProductCalculator.java
+++ 5.Function and Method/ProductCalculator.java
@@ -4,10 +4,11 @@ public class ProductCalculator{
         int prod=num1*num2;
         System.out.println(prod);
     }
-    public static void main (String args []){
+    public static void main (String[] args){
         Scanner sc=new Scanner (System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
         product(a,b);
     }
+    sc.close();
 }
