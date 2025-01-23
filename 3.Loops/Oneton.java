47234435e6a6e6b35f3bd937992b10fa08c82890
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-23 18:39:43 +0530

    Update Oneton.java

--- 3.Loops/Oneton.java
+++ 3.Loops/Oneton.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Oneton{
-public static void main (String args []){
+public static void main (String[] args){
     Scanner sc=new Scanner (System.in);
     int n=sc.nextInt();
     int count=1;
