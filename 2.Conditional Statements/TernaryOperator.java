907af1dd27c5567b345392815261173fcac2887e
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-25 10:57:16 +0530

    Update TernaryOperator.java

--- 2.Conditional Statements/TernaryOperator.java
+++ 2.Conditional Statements/TernaryOperator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class TernaryOperator{
-    public static void main (String args []){
+    public static void main (String[] args){
         Scanner sc=new Scanner (System.in);
         int number=sc.nextInt();
         String type=((number%2)==0)?"Even":"Odd";
