4db15d34faee121f81fa48c6d270ea247a24b90c
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-01-31 19:27:06 +0530

    Update reverseofnumber.java

--- 3.Loops/reverseofnumber.java
+++ 3.Loops/reverseofnumber.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class reverseofnumber {
-    public static void main (String args []){
+    public static void main (String []args){
         Scanner sc=new Scanner (System.in);
         int n=sc.nextInt();
         for (int i=1;n>0;i++){
