164407463557013b6fa009ecbebd6612c681e364
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-04 15:02:48 +0530

    Update Fibonacci.java
    added if statement to handle the 0 and 1 term

--- 1.Variables and Data Types/Fibonacci.java
+++ 1.Variables and Data Types/Fibonacci.java
@@ -4,13 +4,19 @@ public class Fibonacci{
         Scanner sc=new Scanner (System.in);
         System.out.print("Enter the number of terms you want to print :");
         int n=sc.nextInt();
+        sc.colse();
         int first=0;
         int second=1;
+        if(n==0){
+            System.out.println("The Fibonacci Number is : 0");
+        }
+        if(n==1){
+            System.out.println("The Fibonacci Number is : 1");
+        }
         for(int i=2;i<=n;i++){
             int temp=first+second;
             first=second;
             second=temp;
-            
         }
         System.out.println("The Fibonacci Number is :"+second);
         
