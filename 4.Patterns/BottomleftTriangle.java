4cb58d6ee7376f7adee755987cbd77d966adb1fb
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-05 22:01:13 +0530

    Update BottomleftTriangle.java

--- 4.Patterns/BottomleftTriangle.java
+++ 4.Patterns/BottomleftTriangle.java
@@ -1,8 +1,17 @@
 import java.util.*;
 public class BottomleftTriangle {
-    public static void main (String[] args){
-        Scanner sc=new Scanner (System.in);
-        int n=sc.nextInt();
+    //Method -- 1
+    public static void StarPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    //method -- 2
+    public static void BottomleftTriangle(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(i-j>=0){
@@ -11,6 +20,11 @@ public class BottomleftTriangle {
             }
             System.out.println();
         }
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        sc.close();
+        StarPattern(n);
+        BottomleftTriangle(n);
     }
-    
 }
