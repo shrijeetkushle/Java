2ebf4dae6a7b642395072c4ddb96b2e3f749a74c
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-06 10:42:29 +0530

    added Multiplication table code

---
+++ 3.Loops/MultiplicationTable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MultiplicationTable {
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the Table :");
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n + " x " + i + " = " + i * n);
+        }
+        sc.close();
+    }
+}
