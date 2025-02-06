8719ecd0194bef47ed7aa1b78a7decb7aa5788fe
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-06 09:44:28 +0530

    added primeornot code

---
+++ 3.Loops/PrimeOrNot.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class PrimeOrNot {
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the Number :");
+        int n=sc.nextInt();
+        if(n<=1){
+            System.out.println("Not Prime");
+            return;
+        }
+        if(n==2){
+            System.out.println("Prime");
+        }else{
+            boolean isprime=true;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                isprime=false;
+            }
+        }
+        if(isprime==true){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not prime");
+        }
+      }
+    }
+}
