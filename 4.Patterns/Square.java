3425a36d9822c5b3c80114160999b52e59a4b112
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-06 17:48:53 +0530

    Update Square.java

--- 4.Patterns/Square.java
+++ 4.Patterns/Square.java
@@ -1,33 +1,22 @@
+// Pattern Shape
+// * * * *  i=1
+// * * * *  i=2
+// * * * *  i=3
+// * * * *  i=4
+
+//code
 import java.util.*;
 public class Square{
-    // Method --1
-    public static void SquarePattern(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-   // Method --2
-    public static void Square_Pattern(int n){
-         for(int i=1;i<=n;i++){
-             for(int j=1;j<=n;j++){
-                if(i-j<=0){
-                System.out.print("*");
-                }else{
-                System.out.print(" ");
-                }
-             }
-        System.out.println();
-    }
-}
-public static void main (String[] args){
+    public static void main (String[] args){
         Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the Number :");
         int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+             for(int j=1;j<=n;j++){
+                System.out.print("*");
+             }
+             System.out.println();
+            }
         sc.close();
-    SquarePattern(n);
-    Square_Pattern(n);
     }
 }
