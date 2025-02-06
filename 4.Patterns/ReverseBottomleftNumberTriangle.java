7889ac4b9ef3ef69e6e0a041064a9ca74e140587
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-06 17:47:42 +0530

    Update ReverseBottomleftNumberTriangle.java

--- 4.Patterns/ReverseBottomleftNumberTriangle.java
+++ 4.Patterns/ReverseBottomleftNumberTriangle.java
@@ -1,29 +1,23 @@
+// Pattern shape 
+// 4
+// 4 3
+// 4 3 2
+// 4 3 2 1
+
+//code
 import java.util.*;
 public class ReverseBottomleftNumberTriangle {
-    // Method -- 1
-    public static void InvertedStar_Pattern(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-    //Method -- 2
-    public static void ReverseBottomleftNumberTriangle(int n){
-         for(int i=n;i>=1;i--){
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the Number :");
+        int n=sc.nextInt();
+        sc.close();
+        for(int i=n;i>=1;i--){
             for(int j=n;j>=i;j--){
                 System.out.print(j);
             }
             System.out.println();
         }
-    }
-    public static void main (String[] args){
-        Scanner sc=new Scanner (System.in);
-        int n=sc.nextInt();  
-        sc.close();
-        InvertedStar_Pattern(n);
-        ReverseBottomleftNumberTriangle(n);
+            
     }
 }
