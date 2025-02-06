c3ec37a9e8126e3f2bf3274f42610d5cce8d099d
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-06 17:08:40 +0530

    Update BottomleftNumberTriangle.java

--- 4.Patterns/BottomleftNumberTriangle.java
+++ 4.Patterns/BottomleftNumberTriangle.java
@@ -1,3 +1,10 @@
+//pattern shape
+// 1         i-j=0
+// 1 2       i-j=1
+// 1 2 3     i-j=2
+// 1 2 3 4   i-j=3
+
+//code
 import java.util.*;
 public class BottomleftNumberTriangle{
     //Method -- 1
@@ -13,14 +20,18 @@ public class BottomleftNumberTriangle{
     //Method --2
     public static void BottomleftNumberTriangle(int n){
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            //i-j>=0
+            for(int j=1;j<=n;j++){
+                if(i-j>=0){
                 System.out.print(j);
             }
-            System.out.println();
         }
+        System.out.println();
     }
+}
     public static void main (String[] args){
         Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the Number :");
         int n=sc.nextInt();
         sc.close();
         BottomleftNumberTriangle(n);
