08b371ace6989c8b06c6bedfc68807874321bdd1
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-08 21:49:13 +0530

    Update reversegivennumber.java

--- 3.Loops/reversegivennumber.java
+++ 3.Loops/reversegivennumber.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class reversegivennumber {
-    public static void main (String  args []){
+    public static void main (String[]  args){
         Scanner sc=new Scanner (System.in);
         int n=sc.nextInt();
         int rev=0;
