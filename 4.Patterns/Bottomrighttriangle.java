dfcf103e3eba90dfbcb121abcc9d27360fa707e6
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-13 19:57:36 +0530

    Update Bottomrighttriangle.java

--- 4.Patterns/Bottomrighttriangle.java
+++ 4.Patterns/Bottomrighttriangle.java
@@ -38,12 +38,27 @@ public class Bottomrighttriangle {
         }
     
     }
+
+    //method -- 3
+
+    public static void Rotatedhalf_Pyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main (String[] args){
         Scanner sc=new Scanner (System.in);
         System.out.print("Enter the Number :");
         int n=sc.nextInt();
         Bottomright_triangle(n);
         InvertedandRotatedhalf_Pyramid(n);
+        Rotatedhalf_Pyramid(n);
         sc.close();
         
     }
