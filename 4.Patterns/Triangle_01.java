bb5671db0cb63ce47e0910d0184d73f654fa1d13
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-14 00:36:07 +0530

    Add files via upload

---
+++ 4.Patterns/Triangle_01.java
@@ -0,0 +1,28 @@
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+
+//code
+import java.util.*;
+public class Triangle_01{
+        public static void Triangle01(int n){
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=i;j++){
+                        if((i+j)%2==0){
+                            System.out.print("1");
+                        }
+                        else{
+                            System.out.print("0");
+                        }
+                }
+                System.out.println();
+            }
+        }
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the Number :");
+        int n=sc.nextInt();
+        Triangle01(n);
+    }
+}
