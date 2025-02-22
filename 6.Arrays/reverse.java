20ec23dd6c0f2d2998935ae3ba7c0a08aa57ded3
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-22 21:56:54 +0530

    Update reverse.java

--- 6.Arrays/reverse.java
+++ 6.Arrays/reverse.java
@@ -14,6 +14,7 @@ public class reverse{
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
+        
     }
     public static void main (String[] args){
         int arr[]={1,2,3,4,5};
