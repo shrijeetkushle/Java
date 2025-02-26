d85b0270df6171a3cc16a0b0ccfbb49320237512
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-26 20:53:12 +0530

    Update largest.java

--- 6.Arrays/largest.java
+++ 6.Arrays/largest.java
@@ -4,14 +4,15 @@ public class Largest{
         int lar=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
         for(int i=0;i<num.length;i++){
+            // max value=-2147483647
             if(lar<num[i]){
                 lar=num[i];
             }
+           // min value=2147483647
             if(min>num[i]){
                 min=num[i];
             }
-    }
-        
+    }   
     System.out.println(lar+" "+min);
 }
     public static void main (String[] args){
