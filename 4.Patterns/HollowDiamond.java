36e32fad1d93b0f085f5b9ff339ea06efccca2dd
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-02-27 22:22:18 +0530

    Update HollowDiamond.java

--- 4.Patterns/HollowDiamond.java
+++ 4.Patterns/HollowDiamond.java
@@ -9,11 +9,11 @@
 //code
 import java.util.*;
 public class HollowDiamond {
-public static void main (String[] args){
-    Scanner sc=new Scanner (System.in);
-    System.out.print("Enter the Number :");
-    int n=sc.nextInt();
-    int space=1;
+
+    // Method -- 1
+
+    public static void HollowDiamond_pattern1(int n){
+        int space=1;
     
     // print i till last row
 
@@ -55,6 +55,51 @@ public static void main (String[] args){
       }
       System.out.println();
     }
-    sc.close();
-  }
 }
+
+ // Method -- 2
+
+public static void HollowDiamond_pattern2(int n){
+    for(int i=1;i<=n;i++){
+
+        // print first Space
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        if(i==1){
+            System.out.print("*");
+        }else{
+        System.out.print("*");
+        // 2*(i-1) to print inside space
+        for(int k=1;k<2*(i-1);k++){
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    for(int i=n-1;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        if(i==1){
+            System.out.print("*");
+        }else{
+        System.out.print("*");
+        for(int k=1;k<2*(i-1);k++){
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+public static void main (String[] args){
+    Scanner sc=new Scanner (System.in);
+    System.out.print("Enter the Number :");
+    int n=sc.nextInt();
+    
+    }
+}
+    
