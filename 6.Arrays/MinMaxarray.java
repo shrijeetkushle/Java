e9fc216e0428560492f14a2f759aa1a09a6fd9a9
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-03-02 22:04:44 +0530

    Update MinMaxarray.java

--- 6.Arrays/MinMaxarray.java
+++ 6.Arrays/MinMaxarray.java
@@ -1,5 +1,6 @@
 import java.util.*;
-public class Minmaxarray{
+public class MinMaxarray{
+    // Method --1
     public static void Subarray(int num[]){
         int lar=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
@@ -25,7 +26,35 @@ public class Minmaxarray{
         System.out.println("lar is :"+lar);
             System.out.println("min is :"+min);
     }
-    public static void main (String[] args){
+
+
+    // Method --2
+    public static void Pairs(int arr[]){
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                    sum+=arr[k];
+                }
+                System.out.print("Sum is:"+sum);
+                System.out.println();
+                if(sum<min){
+                    min=sum;
+                }
+                if(sum>max){
+                    max=sum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Min Value is : "+min);
+            System.out.println("Max Value is : "+max);
+    }
+    public static void main (String args []){
         int arr[]={1,2,3,4,5};
         Subarray(arr);
 
