bdbb0708fdabf04fcdda70150e79d9bb726ed431
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-03-02 17:32:20 +0530

    Update PairsinArray.java

--- 6.Arrays/PairsinArray.java
+++ 6.Arrays/PairsinArray.java
@@ -1,5 +1,7 @@
 import java.util.*;
 public class PairsinArray{
+
+    // Method --1 
     public static void pairs(int num[]){
         int count=0;
         for(int i=0;i<num.length;i++){
@@ -9,10 +11,23 @@ public class PairsinArray{
                 System.out.print("("+curr+","+num[j]+")");
                 count++; 
             }
-           
-            System.out.printf("%-10d\n",count);
+           // -10d = gives spaces
+            System.out.printf("%-10d\n",count); 
         }
     }
+
+    // Method --2
+    public static void reversePairs(int arr[]){
+    int count =0;
+    for(int i=arr.length-1;i>=0;i--){
+        for(int j=i-1;j>=0;j--){
+            System.out.print("("+i+","+j+")");
+            count++;
+        }
+        System.out.println();
+    }
+    System.out.println(count);
+}
     public static void main (String[] args){
         int arr[]={1,2,3,4,5};
         pairs(arr);
