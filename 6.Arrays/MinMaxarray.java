ef8b2e57b29931493740b035b2b5599ababe7d0b
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-03-14 23:57:28 +0530

    Update MinMaxarray.java

--- 6.Arrays/MinMaxarray.java
+++ 6.Arrays/MinMaxarray.java
@@ -54,8 +54,8 @@ public class MinMaxarray{
         System.out.println("Min Value is : "+min);
         System.out.println("Max Value is : "+max);
     }
-    
 
+    
     public static void main (String args []){
         int arr[]={1,2,3,4,5};
         Subarray(arr);
